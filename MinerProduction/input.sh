3212020f9075384ae7b0a8c5b75d3ce4a279ec03
daniel <ja>
2017-01-05 18:16:00 +0100

    MinerProduction upraven pro input.sh bez proxy

---
+++ MinerProduction/input.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+java -jar miner.jar input/BBCInput.ini &
+java -jar miner.jar input/iDnesInput.ini &
+java -jar miner.jar input/TASSInput.ini &
+java -jar miner.jar input/PalbaInput.ini &

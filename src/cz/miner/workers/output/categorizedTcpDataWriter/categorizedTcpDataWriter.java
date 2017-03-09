4153148f22e95b54c84fe333e98618a5098a684c
daniel <mr.pokorny.d@gmail.com>
2017-03-09 15:03:32 +0100

    Vytvoren categorizedTcpDataWriter.java

---
+++ src/cz/miner/workers/output/categorizedTcpDataWriter/categorizedTcpDataWriter.java
@@ -0,0 +1,10 @@
+package cz.miner.workers.output.categorizedTcpDataWriter;
+
+import cz.miner.workers.Worker;
+/**
+ * @version 0.0.0
+ *          Created by daniel on 9.3.17.
+ */
+public class categorizedTcpDataWriter extends Worker {
+
+}
---
+++ src/cz/miner/workers/output/categorizedTcpDataWriter/categorizedTcpDataWriterConfig.java
@@ -0,0 +1,8 @@
+package cz.miner.workers.output.categorizedTcpDataWriter;
+
+/**
+ * @version 0.0.0
+ *          Created by daniel on 9.3.17.
+ */
+public class categorizedTcpDataWriterConfig {
+}
